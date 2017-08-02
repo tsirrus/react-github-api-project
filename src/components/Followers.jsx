f47fff0aafdba16a3e0028d76bef8a2a221308ce
TSirrus <tsirrus@gmail.com>
2017-08-02 16:07:05 +0000

    Fixed Followers with Infinite

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -3,11 +3,11 @@ import GithubUser from './GithubUser';
 import getGithubToken from '../APIKeys.js';
 import Infinite from 'react-infinite';
 
-var ulStyle = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent : "flex-start"
-};
+// var ulStyle = {
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent : "flex-start"
+// };
 
 class Followers extends React.Component {
     constructor(props){
@@ -15,45 +15,49 @@ class Followers extends React.Component {
         this.state = {
             followers: [],
             page: 1,
-            loading: false
+            loading: false,
+            loadEnd: false
         };
         this.fetchUser = this.fetchUser.bind(this);
     }
     
     fetchUser(){
-        var GITHUB_API_TOKEN = getGithubToken();
-        this.setState({
-            loading: true
-        });
-                
-        fetch(`https://api.github.com/users/${this.props.params.username}/followers?page=${this.state.page}&per_page=25&access_token=${GITHUB_API_TOKEN}`)
-        .then(response => response.json())
-        .then(followers => {
-            console.log("Followers=",followers); //Test
-            
-            if (followers.length > 0) {
-                var currentFollowers = this.state.followers;
-                var formattedFollowers = followers.map(follower => {
-                    return {
-                        username: follower.login,
-                        avatar_url: follower.avatar_url
-                    };
-                });
-                
-                var totalFollowers = currentFollowers.concat(formattedFollowers);
-                this.setState({
-                     followers: totalFollowers,
-                     loading: false,
-                     page: this.state.page + 1
-                });
-            }
-            else {
-                this.setState({
-                    loading: false
-                });
-            }
-            console.log("currentState=",this.state);
-        });
+        if (!this.state.loadEnd) {
+            var GITHUB_API_TOKEN = getGithubToken();
+            this.setState({
+                loading: true
+            });
+                    
+            fetch(`https://api.github.com/users/${this.props.params.username}/followers?page=${this.state.page}&per_page=50&access_token=${GITHUB_API_TOKEN}`)
+            .then(response => response.json())
+            .then(followers => {
+                if (followers.length > 0) {
+                    //console.log("Followers=",followers); //Test
+                    
+                    var currentFollowers = this.state.followers;
+                    var formattedFollowers = followers.map(follower => {
+                        return {
+                            username: follower.login,
+                            avatar_url: follower.avatar_url
+                        };
+                    });
+                    
+                    var totalFollowers = currentFollowers.concat(formattedFollowers);
+                    this.setState({
+                         followers: totalFollowers,
+                         loading: false,
+                         page: this.state.page + 1
+                    });
+                }
+                else {
+                    this.setState({
+                        loading: false,
+                        loadEnd: true
+                    });
+                }
+                //console.log("currentState=",this.state);
+            });
+        }
     }
     
     componentDidMount(){
@@ -62,23 +66,27 @@ class Followers extends React.Component {
     }
     
     summonFollower(follower){
-         return <GithubUser key={follower.username} username={follower.username} avatar_url={follower.avatar_url} />;
+         return (
+             <div key={follower.username} >
+                <GithubUser username={follower.username} avatar_url={follower.avatar_url} />
+             </div>
+            );
     }
     
     render(){
         return (
-            <Infinite 
-                className="followers-page"
-                isInfiniteLoading={this.state.loading}
-                onInfiniteLoad={this.fetchUser}
-                elementHeight={79}
-                infiniteLoadBeginEdgeOffset={100}
-                useWindowAsScrollContainer={true} >
+            <div>
                 <h3>Followers of {this.props.params.username}</h3>
-                <ul style={ulStyle}>
-                    {this.state.followers.map(follower => {return this.summonFollower(follower)})}
-                </ul>
-            </Infinite>
+                <Infinite 
+                    className="followers-page" 
+                    isInfiniteLoading={this.state.loading} 
+                    onInfiniteLoad={this.fetchUser} 
+                    elementHeight={80} 
+                    infiniteLoadBeginEdgeOffset={100} 
+                    useWindowAsScrollContainer={true} >
+                        {this.state.followers.map(follower => {return this.summonFollower(follower)})}
+                </Infinite>
+            </div>
         );
     }
 }
